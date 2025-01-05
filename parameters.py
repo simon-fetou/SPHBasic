8d9cd2e323919968463fad55d13fb261e35e9ae7
Simon <Simonyaka4@gmail.com>
2025-01-05 22:16:37 +0100

    Corrected how the number of particles are fixed (from Fluid patch and not domain

--- parameters.py
+++ parameters.py
@@ -7,16 +7,16 @@ import time
 
 # Domaine et taille du patch initial
 lx_patch =1.
-ly_patch =1.
+ly_patch =1.75
 lx_domain = 2.
 ly_domain = 2.
 
 # Gravite
-g = np.array([0., -0.5])
+g = np.array([0., -9.81])
 
 # Proprietes des fluides
 rho_0 = 1. # densite de reference
-pres0 = 1.
+pres0 = 0
 gamma = 1.
 vel_ref = 0.5*np.sqrt(abs(g[1])*ly_patch)
 c_ref = 10*vel_ref
@@ -25,18 +25,18 @@ k = rho_0*c_ref**2/gamma # coefficient dans l'equation de Tait
 # Parametres numeriques
 h = 0.1 # Smoothing length
 alpha = 0.5  # Coefficient de viscosite artificielle
-h_sur_dx = 0.9 # 
+h_sur_dx = 2 # 
 
 # Constantes du probleme
-dx = h/0.9
+dx = h/h_sur_dx
 dy = dx
-Mx = int(lx_domain/dx)
-My = int(ly_domain/dy)
-Npart = int(Mx**2/2)
+Mx = int(lx_patch/dx)
+My = int(ly_patch/dy)
+Npart = int(Mx*My)
 m_0 = rho_0*(dx**2)
 
 # Parametres temporels
-Tf = 4
+Tf = 2
 dt = h/(8*c_ref)
 N = int(Tf/dt)
-nsave = 5
+nsave = 10
