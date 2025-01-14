d1d5ebf2531892509bd8bcad990aa16a81b4d27a
Simon <Simonyaka4@gmail.com>
2025-01-14 23:34:30 +0100

    Changed h for a fined simu

--- parameters.py
+++ parameters.py
@@ -4,16 +4,18 @@ import numpy as np
 g = np.array([0., -9.81])                   # gravity
 
 #------------------------Domain and Fluid patch parameters--------------------
+x0D = 0.
+y0D = 0.
 lxDomain = 2.
 lyDomain = 2.
-lxFluid = 2.
+lxFluid = lxDomain/2.
 lyFluid = 1.75
-x0F = (lxDomain/2-lxFluid/2)                #centering the fluid patch (x dir)
+x0F = 0.                                    #centering the fluid patch (x dir)
 y0F = 0.                                    # from bottom of recepient (y dir)
 
 #-----------------------------Kernel parameters---------------------------
-h = 0.1                                     # Smoothing length
-h_sur_dx = 1                                # number of fluid particles within h proximity of a particle
+h = 0.05                                     # Smoothing length
+h_sur_dx = 1.4                              # number of fluid particles within h proximity of a particle
 
 #-------------------------------Fluid properties----------------------------
 rho0 = 1.                                   # reference density
