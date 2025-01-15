fdacbbed45ae3b16eaa5c656aa519c7d5ef698c3
Simon <Simonyaka4@gmail.com>
2025-01-15 23:17:19 +0100

    Added the kernel W function definition

--- equations/kernel.py
+++ equations/kernel.py
@@ -3,6 +3,17 @@ import numpy as np
 def length(r):
     return np.sqrt(r.dot(r))
 
+def W(r,h):
+    
+    #facteur de normalisation
+    C=30/(14*np.pi*(h**2))    
+    #paramètre q
+    q=length(r)/h
+    
+    W=C*((2./3-q**2+(q**3)/2)*(q>0)*(q<1)+(((2-q)**3)/6)*((q>1)*(q<2))+0*(q>2))
+    
+    return W
+
 def gradW(r, h):              # gradient Cubic kernel 2H
     C=30/(14*np.pi*h**2)      #on est en 2D
     q=length(r)/h
